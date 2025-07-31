4824ea9a2a6e44e80a9f2b31d9224eb07ca83453
Brook Dev <arivazhangan14@gmail.com>
2025-07-30 23:41:05 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let nextBtn = document.getElementById('next');
+let prevBtn = document.getElementById('prev');
+let backBtn = document.getElementById('back');
+let seeBtn = document.querySelectorAll('.see');
+let carousel = document.querySelector('.container');
+let swiper = document.querySelector('.container .swiper');
+
+
+
+
+nextBtn.onclick = function(){
+    showSlider('next');
+}
+prevBtn.onclick = function(){
+    showSlider('prev');
+}
+
+const showSlider =(type) => {
+    let items =document.querySelectorAll('.item');
+    if(type == 'next'){
+        swiper.appendChild(items[0]);
+    } else{
+        let last= items[items.length -1];
+        swiper.prepend(last);
+    }
+}
+
+seeBtn.forEach(button => {
+    button.onclick = function(){
+        carousel.classList.add('show');
+        console.log('See button clicked:', button);
+    }
+});
+
+backBtn.onclick = function(){
+    carousel.classList.remove('show');
+    console.log('Back button clicked');
+}
+
